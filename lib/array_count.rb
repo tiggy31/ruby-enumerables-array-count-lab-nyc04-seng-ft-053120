396563d103cdec2a13380fcd0ece680fbfc41de4
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 19:42:09 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,14 @@
+   require "pry"
 def count_strings(array)
+ array.count { |element| element.class == String}
+   #binding.pry
   # Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array = array.grep(String)
+array.count{ |element| element.empty?  }   
+  #Return the total number of EMPTY strings in the provided array using the count enumerable
+  
+  
 end
