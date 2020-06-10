468daf09487341b7c93735b80a2b95f27f9c12f0
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 18:53:57 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,7 @@
+   require "pry"
 def count_strings(array)
   array.count do |element|
+    binding.pry
     element.String?
   # Return the total number of strings in the provided array using the count enumerable
 end
