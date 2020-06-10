b4fe52dcab42fbb23712d0a47b8401325c6e0000
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 19:24:09 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -7,7 +7,7 @@ end
 
 def count_empty_strings(array)
   
- array.split.count { |element| element.empty? }
+ array.split.grep(String).count { |element| element.empty? }
   #Return the total number of EMPTY strings in the provided array using the count enumerable
   
   
